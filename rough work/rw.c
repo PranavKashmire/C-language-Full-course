185eb8beb80c5980704c51322ef13f7226594bf8
Pranav Kashmire <pranav.kashmire09@gmail.com>
2024-11-11 18:57:39 +0530

    Add files via upload

---
+++ rough work/rw.c
@@ -0,0 +1,213 @@
+
+//#include<stdio.h>
+
+//int main(){
+  // int lenght , breadth;
+   //printf("the length is:");
+  // scanf("%d", &lenght);
+
+  // printf("the bradth is:");
+  // scanf("%d",&breadth);
+  // printf("the area is %d",lenght*breadth);
+  // return 0;
+   
+//} 
+
+ 
+/*
+#include<stdio.h>
+
+int main(){
+   int radius;
+   printf("the radius of circle is:");
+   scanf("%d", &radius);
+   printf("the area of circle is : %d", 3.14*(radius)^2);
+   return 0;
+}
+
+*/
+
+
+/*
+#include<stdio.h>
+
+int main(){
+  int bredth=45 , length=74;
+
+  printf ("the area of rectangle is %d", length*bredth);
+  return 0;
+}
+ 
+
+*/
+
+
+/*
+#include<stdio.h>
+
+int main(){
+  int radius;
+  printf("the radius of circle is %d");
+  scanf("%d", &radius);
+printf("the area of circle is %f", 3.14*radius*radius);
+  return 0;
+}
+
+*/
+/*
+#include<stdio.h>
+
+int main(){
+  int lenght , bredth;
+printf("the lenght of rectangle is : ");
+scanf("%d",  &lenght);
+
+printf("the bredth of rectangle is :");
+scanf("%d",  &bredth);
+
+printf("the area of rectangle is : %d", lenght*bredth);
+  return 0;
+}
+
+*/
+
+/*
+#include<stdio.h>
+
+int main(){
+  int a=14;
+  if(a>10){
+    printf("the number is greater than 10");
+  }
+  else{
+    printf(" the number is less");
+    }
+  return 0;
+}
+
+*/
+/*
+#include<stdio.h>
+
+int main(){
+  int num;
+  printf("the value of num is:");
+  scanf("%d",&num);
+if(num>0 && num<5){
+  printf("the number lies between 1 and 5");
+}
+else if(num>5 && num<10){
+  printf("the number lies between 5 and 10");
+}
+
+else{
+  printf("the number is greater than 10");
+}
+
+  return 0;
+}
+
+*/
+ /*
+
+#include<stdio.h>
+
+int main(){
+  int m1 , m2 , m3 ;
+  printf("enter the marks of sunbj1:");
+  scanf("%d", &m1);
+  printf("enter the marks of sunbj2:");
+  scanf("%d", &m2);
+  printf("enter the marks of sunbj3:");
+  scanf("%d", &m3);
+
+  int avg;
+  avg = (m1+m2+m3)/3;
+  //printf("the average is :%d" , avg);
+
+  if (avg>40 && m1>33 && m2>33 && m3>33){;
+  printf("The student is pass!!");
+  }
+  else{
+     printf("the student  has failed");
+  }
+  return 0;
+}
+
+*/
+
+/*
+
+#include<stdio.h>
+
+int main(){
+  int income,tax;
+  printf("Enter your income:");
+  scanf("%f", &income);
+
+  tax= (20*100)/ income ;
+
+
+  if (income<2.5){
+    printf("Since Your income is less than 2.5 L no tax");
+  }
+
+  if (income>2.5 && income<5){
+
+    printf("tax will be :%d", tax);
+  }
+
+  return 0;
+}
+*/
+
+//while loops
+
+/*
+#include<stdio.h>
+
+int main(){
+  int num=0;
+  while(num<21){
+    if(num>=10)
+    printf("the value  %d\n ", num);
+    num++;
+    }
+  return 0;
+}
+
+*/
+
+//for loop
+
+
+/*
+ #include<stdio.h>
+ 
+ int main(){
+  for(int a=0; a<10 ;a++){
+  printf("the value is %d\n", a);
+  if(a==6){
+    break;
+  }
+  }
+
+  return 0;
+ } 
+ */
+
+/*
+#include<stdio.h>
+
+int main(){
+  int num=5 , i=0 ;
+  i++;
+  while(i<11){
+    printf("table" ,num*i);
+  }
+  return 0;
+}
+
+*/
+
+
